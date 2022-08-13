5b8e5bb52037eb75f5e510aa1d1387d2f3c27c2f
cwgroppe <christophergroppe@gmail.com>
2022-08-13 02:12:51 -0400

    Update Problems001.scala

--- Problems001.scala
+++ Problems001.scala
@@ -1,5 +1,26 @@
 object Problems001{
-  //1
+  //#1 - Find the sum of all the multiples of 3 or 5 below 1000.
   println((1 to 999).toList.filter(x => x % 5 == 0 || x % 3 == 0).sum)
   //233168
+  
+  //#2 - By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+  def fib(num: Int): BigInt = {
+    val buf = scala.collection.mutable.ListBuffer.empty[BigInt]
+    @scala.annotation.tailrec
+    def fibFcn(n: Int, acc1: BigInt, acc2: BigInt): BigInt = n match {
+      case 0 => acc1
+      case 1 => acc2
+      case _ => {
+        val x = acc1 + acc2
+        if(x % 2 == 0){buf += x} 
+        fibFcn(n - 1, acc2, x)
+      }
+    }
+    val f = fibFcn(num, 0, 1)
+    println(buf.toList.sum)
+    f
+  }
+  fib(33)
+  //4613732
 }
+
