2d6a70ce9d40979f3b85db1101c6cac57fd8603e
cwgroppe <christophergroppe@gmail.com>
2022-08-13 01:00:28 -0400

    Update Problems001.scala

--- Problems001.scala
+++ Problems001.scala
@@ -1,2 +1,4 @@
-println((1 to 999).toList.filter(x => x % 5 == 0 || x % 3 == 0).sum)
-//233168
+object Problems001{
+  println((1 to 999).toList.filter(x => x % 5 == 0 || x % 3 == 0).sum)
+  //233168
+}
